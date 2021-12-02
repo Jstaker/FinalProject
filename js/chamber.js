e33f2f385993c9b179fd8336eb3b7b30f43bd1e8
Jstaker <90649467+Jstaker@users.noreply.github.com>
2021-12-01 19:15:58 -0700

    working on home page

---
+++ js/chamber.js
@@ -0,0 +1,18 @@
+//display last updated
+var weekday = new Array(
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    );
+var months = new Array(
+    "January", "February","March", "April", "May", "June", "July", "August", "September","October", "November", "December"
+    );
+const year = new Date().getFullYear();
+const lastModified = new Date(document.lastModified);
+var day = lastModified.getDay();
+var day = weekday[day];
+var month = lastModified.getMonth();
+var month = months[month];
+
+var lastModifiedFormatted = day + ', ' +  month + " " + lastModified.getDate() + " " + lastModified.getFullYear();
+
+// Display copyright year and date last modified to footer of HTML document.
+document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
